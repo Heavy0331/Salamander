014cbdd3acc9c725ca4ea8513608555e75cc95dc
Daniel Underwood <underthewood34@gmail.com>
2023-08-27 01:35:26 -0500

    improve gitignore

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Salamander',
+    version='1.0',
+    description='A simple minute taker',
+    author='KrakenDev and Heavy0331',
+    packages=['salamander'],
+
+    install_requires=[
+        'customtkinter',
+    ]
+
+)
